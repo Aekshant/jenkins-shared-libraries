eaf23cfba57879b51613135f24f10669244dcef7
Kalyan Dahake <75876845+Aekshant@users.noreply.github.com>
2024-09-29 08:50:37 +0530

    Update clone.groovy
    Clone error done

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
 def call(String url, String branch, String CredId){
-  git branch: "${url}", url: "${branch}", credentialsId: "${CredId}"
+  git branch: "${branch}", url: "${url}", credentialsId: "${CredId}"
   echo 'happily Clone'
 }
